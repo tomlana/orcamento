a8c391fd83c1b37d3a66e1cbc439c70dcc8b959a
Thomas Lana <61917083+tomlana@users.noreply.github.com>
2021-03-15 15:35:26 -0300

    Componentes do site

---
+++ components/Menu.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+  } from 'reactstrap';
+
+
+  const Menu = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggle = () => setIsOpen(!isOpen);
+    return (
+        <div>
+        <Navbar color="primary" dark expand="md">
+            <Container>
+          <NavbarBrand href="/">Orçamento</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/orcamento">Orçamento</NavLink>
+              </NavItem>
+            </Nav>
+    
+          </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+}
+
+export default Menu;
